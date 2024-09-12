105757178794a69e2be87847fc29c3f9fec41f6c
Pranish Ghimire <pranishghimire109@gmail.com>
2024-09-12 11:03:20 -0500

    added js

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Automatically scroll the visual out after page loads
+    setTimeout(() => {
+        document.querySelector('.scroll-visual').style.display = 'none';
+    }, 2000); // 2 seconds for the scroll effect
+});
+
